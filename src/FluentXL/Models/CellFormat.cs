1319fd4bfb0ea95eb2341f5777b8372e6c447b84
Arias Emilio <ariasemis@gmail.com>
2018-06-27 18:15:24 -0300

    added cell format model

--- src/FluentXL/Models/CellFormat.cs
+++ src/FluentXL/Models/CellFormat.cs
@@ -1,12 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace FluentXL.Models
+﻿namespace FluentXL.Models
 {
     public class CellFormat
     {
-        //TODO: private constructor until feature is done
-        private CellFormat() { }
+        public CellFormat(
+            uint id,
+            uint? formatId = null,
+            uint? fontId = null,
+            uint? fillId = null,
+            uint? borderId = null,
+            uint? numberFormatId = null,
+            bool? hasPivotButton = null,
+            bool? hasQuotePrefix = null,
+            Alignment alignment = null,
+            Protection protection = null)
+        {
+            Id = id;
+            FormatId = formatId;
+            FontId = fontId;
+            FillId = fillId;
+            BorderId = borderId;
+            NumberFormatId = numberFormatId;
+            HasPivotButton = hasPivotButton;
+            HasQuotePrefix = hasQuotePrefix;
+            Alignment = alignment;
+            Protection = protection;
+        }
+
+        public uint Id { get; }
+
+        public uint? FormatId { get; }
+
+        public uint? FontId { get; }
+
+        public uint? FillId { get; }
+
+        public uint? BorderId { get; }
+
+        public uint? NumberFormatId { get; }
+
+        public bool? HasPivotButton { get; }
+
+        public bool? HasQuotePrefix { get; }
+
+        public Alignment Alignment { get; }
+
+        public Protection Protection { get; }
     }
 }
---
+++ src/FluentXL/Models/Protection.cs
@@ -0,0 +1,15 @@
+﻿namespace FluentXL.Models
+{
+    public class Protection
+    {
+        public Protection(bool locked, bool hidden)
+        {
+            Locked = locked;
+            Hidden = hidden;
+        }
+
+        public bool Locked { get; }
+
+        public bool Hidden { get; set; }
+    }
+}
