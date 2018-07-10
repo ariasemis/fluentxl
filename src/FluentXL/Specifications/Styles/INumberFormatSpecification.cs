632fe8ed923f03d2a8861cf45284658efa2abfb1
Arias Emilio <ariasemis@gmail.com>
2018-07-10 16:42:19 -0300

    implemented number format specification

--- src/FluentXL/Specifications/Styles/INumberFormatSpecification.cs
+++ src/FluentXL/Specifications/Styles/INumberFormatSpecification.cs
@@ -4,7 +4,7 @@ namespace FluentXL.Specifications.Styles
 {
     public interface INumberFormatSpecification
     {
-        IBuilderSpecification<Border> WithFormat(string format);
+        IBuilderSpecification<NumberFormat> WithFormat(string format);
 
         IBuilderSpecification<NumberFormat> WithFormat(StandardNumberFormat format);
     }
---
+++ src/FluentXL/Specifications/Styles/NumberFormatSpecification.cs
@@ -0,0 +1,32 @@
+﻿using FluentXL.Models;
+using System;
+
+namespace FluentXL.Specifications.Styles
+{
+    public class NumberFormatSpecification : INumberFormatSpecification, IBuilderSpecification<NumberFormat>
+    {
+        private Func<uint, NumberFormat> BuildFunc { get; set; }
+
+        private NumberFormatSpecification() { }
+
+        public static INumberFormatSpecification New()
+            => new NumberFormatSpecification();
+
+        public NumberFormat Build(IBuildContext context)
+        {
+            var id = context.Stylesheet.GenerateNumberFormatId();
+
+            var numberFormat = BuildFunc(id);
+
+            context.Stylesheet.Add(numberFormat);
+
+            return numberFormat;
+        }
+
+        public IBuilderSpecification<NumberFormat> WithFormat(string format)
+            => new NumberFormatSpecification { BuildFunc = id => new NumberFormat(id, format) };
+
+        public IBuilderSpecification<NumberFormat> WithFormat(StandardNumberFormat format)
+            => new NumberFormatSpecification { BuildFunc = _ => new NumberFormat((uint)format, string.Empty) };
+    }
+}
